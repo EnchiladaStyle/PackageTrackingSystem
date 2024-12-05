17a463033bdec763c3990c1e81505009506a6681
Maximoolean <Maximoolean@gmail.com>
2024-12-05 12:04:40 -0700

    updating the id request receiver to send to database

--- package_tracking_system/src/pkg_id_request_receiver.erl
+++ package_tracking_system/src/pkg_id_request_receiver.erl
@@ -7,6 +7,10 @@
 %% gen_server callbacks
 -export([init/1, handle_cast/2, handle_call/3, handle_info/2, terminate/2, code_change/3]).
 
+%% Riak Connection
+-define(RIAK_HOST, "137.184.176.152").
+-define(RIAK_PORT, 8087).
+
 %% API to start the worker
 start_link() ->
     gen_server:start_link(?MODULE, [], []).
@@ -17,7 +21,14 @@ store_package(Pid, PackageId) ->
 
 %% Callbacks
 init([]) ->
-    {ok, []}.
+    %% Start the Riak client application
+    ok = application:start(riakc),
+
+    %% Connect to the database
+    {ok, RiakPid} = riakc_pb_socket:start_link(?RIAK_HOST, ?RIAK_PORT),
+
+    %% Store the Pid in ther server's state
+    {ok, #{riak_pid => RiakPid}}.
 
 handle_cast({store_package, PackageId}, State) ->
     %% Default data
@@ -27,9 +38,22 @@ handle_cast({store_package, PackageId}, State) ->
     StateData = <<"In Transit">>,
     %% Data structure
     PackageData = {WarehouseId, Latitude, Longitude, StateData},
+
+    %% Convert data to a binary term for Riak
+    Bucket = <<"packages">>,
+    Key = binary:encode_unsigned(PackageId), %% Package Id is the Key
+    Value = term_to_binary(PackageData),
     
-    %% Simulate storing in Riak (replace with actual Riak API call)
-    riak:put(PackageId, PackageData),
+    %% Get Riak connection from the state
+    RiakPid = maps:get(riak_pid, State),
+
+    %% Store in database
+    case riakc_pb_socket:put(RiakPid, riakc_obj:new(Bucket, Key, Value)) of
+        ok ->
+            io:format("Package ~p stored successfully in database~n", [PackageId]);
+        {error, Reason} ->
+            io:format("Failed to store package~p in database: ~p~n", [PackageId, Reason])
+        end,
     
     {noreply, State}.
 
@@ -39,7 +63,14 @@ handle_call(_Request, _From, State) ->
 handle_info(_Info, State) ->
     {noreply, State}.
 
-terminate(_Reason, _State) ->
+terminate(_Reason, State) ->
+    %% Stop the Riak connection
+    RiakPid = maps:get(riak_pid, State),
+    riakc_pb_socket:stop(RiakPid),
+
+    %% Stop the Riak application
+    application:stop(riakc),
+
     ok.
 
 code_change(_OldVsn, State, _Extra) ->
